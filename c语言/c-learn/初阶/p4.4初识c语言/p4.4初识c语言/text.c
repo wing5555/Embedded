2bb4d270b661acc688dc7ef473ee5ce4575cf96d
wing5555 <1747098083@qq.com>
2024-02-27 20:59:18 +0800

    无

--- c语言/c-learn/初阶/p4.4初识c语言/p4.4初识c语言/text.c
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-//char g_val[] = "abc";
-//int g_val = 19;
-static int ADD(int x,int y)
-{
-	int z = x + y;
-	return z;
-}
--- c语言/c-learn/初阶/函数/函数/text.c
+++
@@ -1,7 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-int Add(int x, int y)
-{
-	int sum = 0;
-	sum = x + y;
-	return sum;
-}
