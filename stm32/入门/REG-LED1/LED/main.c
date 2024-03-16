bfc00f051dd8c619dcbd0b4d5d9ae4b311fa0ee7
wing5555 <1747098083@qq.com>
2024-03-16 14:04:59 +0800

    32

--- stm32/入门/REG-LED1/LED/main.c
+++ stm32/入门/REG-LED1/LED/main.c
@@ -18,15 +18,15 @@
 
 int main(void)
 {
-	GPIO_InitTypeDef GPIO_InitStruct;
+//	GPIO_InitTypeDef GPIO_InitStruct;
 	RCC -> APB2ENR |= (0x01 << 3);
-//	GPIOB -> CRL &= ~(0x0f << (4*1));
-//	GPIOB -> CRL |= (0x01 << (4*1));
-	GPIO_InitStruct.GPIO_Mode = GPIO_Pin1;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOB,&GPIO_InitStruct);
-	GPIO_ResetBits(GPIOB,GPIO_Pin1);
+	GPIOB -> CRL &= ~(0x0f << (4*0));
+	GPIOB -> CRL |= (0x01 << (4*0));
+//	GPIO_InitStruct.GPIO_Pin = GPIO_Pin0;
+//	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+//	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+//	GPIO_Init(GPIOB,&GPIO_InitStruct);
+	GPIO_ResetBits(GPIOB,GPIO_Pin0);
 }
 
 void SystemInit(void)
